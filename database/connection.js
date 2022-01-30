762d5a9051f00d0caa8cec4d4249646f301c8675
unknown <michaelmedewase@gmail.com>
2022-01-31 00:55:00 +0100

    j

--- database/connection.js
+++ database/connection.js
@@ -6,6 +6,7 @@ const Connection = new Sequelize(process.env.DB_NAME,
     process.env.DB_USER,process.env.DB_PASSWORD,{
     host: process.env.DB_HOST,
     dialect:process.env.DB_DIALECT,
+    port:process.env.DB_PORT,
     pool: {
         max: 5,
         min: 0,
