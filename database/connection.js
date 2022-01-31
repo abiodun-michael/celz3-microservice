9ee237fd4e3ae8b2a19ab542cb9eb8a62b3b8440
unknown <michaelmedewase@gmail.com>
2022-01-31 02:03:51 +0100

    j

--- database/connection.js
+++ database/connection.js
@@ -2,18 +2,23 @@ require('dotenv').config()
 const Sequelize = require('sequelize')
 
 
-const Connection = new Sequelize(process.env.DB_NAME,
-    process.env.DB_USER,process.env.DB_PASSWORD,{
+const Connection = new Sequelize({
     host: process.env.DB_HOST,
     dialect:process.env.DB_DIALECT,
     port:process.env.DB_PORT,
+    username:process.env.DB_USER,
+    password:process.env.DB_PASSWORD,
     pool: {
         max: 5,
         min: 0,
         acquire: 30000,
         idle: 10000
       },
-      ssl:{}
+      dialectOptions:{
+          ssl:{
+              rejectUnauthorized:false
+          }
+      }
 })
 
 Connection.authenticate().then((response)=>{
