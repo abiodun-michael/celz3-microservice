304aeb14d7de4c552feae7a1b45f8821871aad43
unknown <michaelmedewase@gmail.com>
2022-02-05 17:25:12 +0100

    jfhf

--- server.js
+++ server.js
@@ -106,9 +106,8 @@ const io = new Server(appServer,{
 })
 
 io.on("connection", (socket) => {
-  const cookies = socket.request.headers
-  socket.on("welcome","Welcoe guys")
-  socket.emit("connected",cookies)
+    const cookies = socket.request.headers
+    socket.emit("connected",cookies)
 });
 
 }
