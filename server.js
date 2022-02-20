61e47d8dbdcfef9e0ffd7d32d0defa5cc1dffe7d
unknown <michaelmedewase@gmail.com>
2022-02-20 13:41:55 +0100

    kd

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const NodeMediaServer = require('node-media-server');
 const ffmpeg = require('@ffmpeg-installer/ffmpeg');
+const express = require("express")
 
+const app = express()
 
 const config = {
   rtmp: {
@@ -22,7 +24,7 @@ const config = {
         app: 'live',
         ac: 'aac',
         hls: true,
-        hlsFlags: '[hls_time=2:hls_list_size=3:hls_flags=delete_segments]',
+        hlsFlags: '[hls_time=10:hls_list_size=3:hls_flags=delete_segments]',
         dash: true,
         dashFlags: '[f=dash:window_size=3:extra_window_size=5]'
       }
@@ -31,46 +33,61 @@ const config = {
 };
  
 var nms = new NodeMediaServer(config)
-nms.run();
 
-nms.on('preConnect', (id, args) => {
-  console.log('[NodeEvent on preConnect]', `id=${id} args=${JSON.stringify(args)}`);
-  // let session = nms.getSession(id);
-  // session.reject();
-});
+nms.run()
 
-nms.on('postConnect', (id, args) => {
-  console.log('[NodeEvent on postConnect]', `id=${id} args=${JSON.stringify(args)}`);
-});
 
-nms.on('doneConnect', (id, args) => {
-  console.log("stream done connecting");
-});
+// app.get("/start",(req,res)=>{
+  
+// })
 
-nms.on('prePublish', (id, StreamPath, args) => {
- console.log('Stream prepublish')
-  // let session = nms.getSession(id);
-  // session.reject();
-});
+// nms.run();
 
-nms.on('postPublish', (id, StreamPath, args) => {
-  console.log('stream postpublish');
-});
 
-nms.on('donePublish', (id, StreamPath, args) => {
-  console.log("Stream donepublish");
-});
+// nms.on('preConnect', (id, args) => {
+//   console.log('[NodeEvent on preConnect]', `id=${id} args=${JSON.stringify(args)}`);
+//   // let session = nms.getSession(id);
+//   // session.reject();
+// });
 
-nms.on('prePlay', (id, StreamPath, args) => {
-  console.log("stream preplay");
-  // let session = nms.getSession(id);
-  // session.reject();
-});
+// nms.on('postConnect', (id, args) => {
+//   console.log('[NodeEvent on postConnect]', `id=${id} args=${JSON.stringify(args)}`);
+// });
 
-nms.on('postPlay', (id, StreamPath, args) => {
-  console.log("stream post play");
-});
+// nms.on('doneConnect', (id, args) => {
+//   console.log("stream done connecting");
+// });
 
-nms.on('donePlay', (id, StreamPath, args) => {
-  console.log("stream is done playing");
-});
+// nms.on('prePublish', (id, StreamPath, args) => {
+//     console.log('Stream prepublish')
+//   // let session = nms.getSession(id);
+//   // session.reject();
+// });
+
+// nms.on('postPublish', (id, StreamPath, args) => {
+//   console.log('stream postpublish');
+// });
+
+// nms.on('donePublish', (id, StreamPath, args) => {
+//   console.log("Stream donepublish");
+// });
+
+// nms.on('prePlay', (id, StreamPath, args) => {
+//   console.log("stream preplay");
+//   let session = nms.getSession(id);
+//   console.log(args)
+//   // session.reject();
+// });
+
+// nms.on('postPlay', (id, StreamPath, args) => {
+//   console.log("stream post play");
+// });
+
+// nms.on('donePlay', (id, StreamPath, args) => {
+//   console.log("stream is done playing");
+// });
+
+
+// app.listen(3000,()=>{
+//   console.log("Server is running")
+// })
